e09dfaf74b5a99e4392c42eaff3494093f37f86e
marinpettersen <anizsubhan@gmail.com>
2024-08-20 14:15:17 +0700

    add railway

--- config/database.js
+++ config/database.js
@@ -1,11 +1,25 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'express_db',
+//     password: 'postgres',
+//     port: 5432,
+//     connectTimeout: 5000,
+// });
+
+// module.exports = pool;
+
+const { Pool } = require('pg');
+require('dotenv').config();  // Mengimpor dotenv untuk memuat variabel lingkungan dari file .env
+
+// Konfigurasi Pool dengan URL dari variabel lingkungan, jika tersedia
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'express_db',
-    password: 'postgres',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:wBTxaOloXiwqwIGCsZxSDBLEIbIpAMWs@autorack.proxy.rlwy.net:41127/railway',
+    ssl: process.env.DATABASE_URL ? {
+        rejectUnauthorized: false,
+    } : false,
     connectTimeout: 5000,
 });
 
