620ef83ea20df7d325f3978d56ce17fb9a70a8a0
marinpettersen <anizsubhan@gmail.com>
2024-08-22 12:55:45 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
